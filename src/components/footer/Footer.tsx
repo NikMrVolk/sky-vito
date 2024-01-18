7064143c7c6bbccf805d86b12f45d3ddcdee612b
NickZavadski <nick.mr.volk@gmail.com>
2024-01-18 15:24:08 +0300

    Revert "fix: mobile footer moved to items list"
    This reverts commit fef1d3aa8ac8bc215d98d41e5c4ffc71fa3ff25d.

--- src/components/footer/Footer.tsx
+++ src/components/footer/Footer.tsx
@@ -5,7 +5,7 @@ import { MAIN_ROUTE, PROFILE_ROUTE } from '@/utils/constants/routes'
 
 export default function Footer() {
     return (
-        <footer className="fixed bottom-0 flex w-full items-center justify-center gap-14 bg-white py-1.5 shadow-mobileFooter sm:hidden">
+        <footer className="flex items-center justify-center gap-14 py-1.5 shadow-mobileFooter sm:hidden">
             <Link href={MAIN_ROUTE}>
                 <Image src="/images/mobileFooter/home.png" alt="home" width={28} height={23} />
             </Link>
