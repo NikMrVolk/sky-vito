fa50cf754993f970afca2b346a3c14757f983fc5
NickZavadski <nick.mr.volk@gmail.com>
2024-01-19 15:34:01 +0300

    prog: change file structure (add login and registration with another layout)

--- src/app/(app)/layout.tsx
+++ src/app/(app)/layout.tsx
@@ -6,7 +6,7 @@ import Footer from '@/components/footer/Footer'
 import Header from '@/components/header/Header'
 import { cn } from '@/lib/className'
 
-import './globals.css'
+import '../globals.css'
 
 const roboto = Roboto({
     weight: ['400', '700'],
