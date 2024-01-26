55fadb7aa7f58463020ac5cfb36c66a39d13d73a
NickZavadski <nick.mr.volk@gmail.com>
2024-01-26 19:27:31 +0300

    prog: show user items in profile page

--- src/app/(app)/profile/page.tsx
+++ src/app/(app)/profile/page.tsx
@@ -1,14 +1,29 @@
+'use client'
+
+import { useQuery } from '@tanstack/react-query'
+
 import IsAuthComponent from '@/components/common/IsAuthComponent'
 import BlockWrapper from '@/components/common/wrappers/BlockWrapper'
 import ItemsList from '@/components/item/ItemsList'
 import ProfileInfo from '@/components/profile/ProfileInfo'
+import { itemsService } from '@/services/items/items.service'
+import { QueryKeys } from '@/utils/constants/reactQuery'
 
 export default function Profile() {
+    const { data, isLoading } = useQuery({
+        queryKey: [QueryKeys.GET_USER_ADS],
+        queryFn: itemsService.getUserItems,
+    })
+
     return (
         <BlockWrapper>
             <IsAuthComponent />
             <ProfileInfo />
-            <ItemsList title="Мои товары" classes={{ title: 'mb-7.5' }} />
+            {isLoading ? (
+                <>Загрузка...</>
+            ) : (
+                <ItemsList title="Мои товары" items={data?.data} classes={{ title: 'mb-7.5' }} />
+            )}
         </BlockWrapper>
     )
 }
